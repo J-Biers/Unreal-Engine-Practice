cb3bfe1c64c33cdb60364fa51ad475987a3aba15
biermanj <biermanj@purdue.edu>
2021-02-20 11:53:10 -0600

    Making some changes

---
+++ Triple-X/TripleX.cpp
@@ -0,0 +1,17 @@
+//Pre-Processor Directives. Identified by '#'
+#include <iostream> // <- Header file (in C followed by .h)
+#include <ctime>
+
+
+bool playLevel(int difficulty) {
+
+
+	return true;
+}
+
+int main()
+{
+	playLevel(1);
+	std::cout << "hi\n";
+	return 0;
+}
